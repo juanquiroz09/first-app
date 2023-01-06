921f0aed0d07e08bf86b89142e20eda0cd27292d
Juan Quiroz <juan.quiroz1009@gmail.com>
2023-01-06 13:30:12 -0500

    first-commit

---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado',
+  templateUrl: './empleado.component.html',
+  //template: "<p>aqui iria un empleado</p>",
+  styleUrls: ['./empleado.component.css'],
+  //styles: ["p{background-color: red;}"]
+})
+export class EmpleadoComponent {}
